006a80c33f4a6157249d50d38cf7ffcee154df50
Zhenbang(Bruce) Wang <bruce1edward@gmail.com>
2022-04-21 19:13:04 -0400

    Add files via upload

--- coxph with baseline unknown 1.R
+++ coxph with baseline unknown 1.R
@@ -19,55 +19,92 @@ y <- exp(drop(mu)) * xi_mult#
 yperm <- y
 yperm[1:k] <- y[sample(1:k)]
 cens <- (yperm >= 3) # 11% censored
-
-
 alpha <- 0.5
 maxiter <- 1000
 tol <- 1E-4
 objs <- numeric(maxiter)
 
-
+########################Naive###################################
 creg_naive <- coxph(Surv(yperm, event = 1 - cens) ~ X - 1)
 mu <- X %*% creg_naive$coef
 
-###################### smoothed #################
+# now estimate null component
 
-lambdahat_0_g <- 4*yperm^3#lambdahat(yperm, 0.1)
-Lambdahat_0_g <- yperm^4#Lambdahat(yperm, 0.1)
+survf0 <- survfit(Surv(yperm, event = 1 - cens) ~ 1)
+Delta <- c(survf0$cumhaz[1], diff(survf0$cumhaz))
+times <- survf0$time
+
+# kernel estimation of the baseline hazard function
+lambdahat <- function(t, h){
+  
+  rowSums(sweep(dnorm(outer(t, times, "-"), sd = h), MARGIN = 2, FUN = "*", STATS = Delta))
+  
+}
+
+# corresponding cumulative hazard function
+Lambdahat <- function(t, h){
+  
+  rowSums(sweep(pnorm(outer(t, times, "-"), sd = h), MARGIN = 2, FUN = "*", STATS = Delta))
+  
+}
+
+lambdastar <- function(t) rowMeans(sweep(dweibull(outer(t, gamma, FUN = "/"), shape = 1/sigma), MARGIN = 2, FUN = "/", STATS = gamma)) /( (1 - rowMeans(pweibull(outer(t, gamma, FUN = "/"), shape = 1/sigma))) )
+
+Lambdastar <- function(t) -log((1 - rowMeans(pweibull(outer(t, gamma, FUN = "/"), shape = 1/sigma))) )
+
+fy <- lambdahat(yperm, 0.1)^(1-cens) * exp(-Lambdahat(yperm, 0.1))^cens
 
 ################## initialize baseline function #############
-lambdahat_0 <- lambdahat_0_g
-Lambdahat_0 <- Lambdahat_0_g
+#################baseline for corrected matched data#################
+lambdahat_0 <- 4*yperm^3
+Lambdahat_0 <- yperm^4
+#################baseline for  mismatched data################
+g_lambdahat_0 <- lambdastar(yperm)# lambdahat(yperm, 0.1) #lambdastar(yperm)
+g_Lambdahat_0 <- Lambdastar(yperm)# Lambdahat(yperm, 0.1) #Lambdahat(yperm)
 
 fymu <- function(mu, cens, l0, L0) (exp(mu) * l0)^(1 - cens) *  exp(-exp(mu) * L0)
-fy <- function(l0, L0) l0^(1-cens) * exp(-L0)^cens
-nloglik_marginal <- function(mu, cens, alpha, l0, L0) sum(-log((1-alpha) * fymu(mu, cens, l0, L0) + alpha * fy(lambdahat_0_g, Lambdahat_0_g)), na.rm = TRUE)
+fy <- function(g_l0, g_L0) g_l0^(1-cens) * exp(-g_L0)^cens
+nloglik_marginal <- function(mu, cens, alpha, l0, L0, g_l0, g_L0) sum(-log((1-alpha) * fymu(mu, cens, l0, L0) + alpha * fy(g_l0, g_L0)), na.rm = TRUE)
 # update weights and alpha; then repeat
 iter <- 1
-objs[iter] <- nloglik_marginal(mu, cens, alpha, lambdahat_0, Lambdahat_0)
+objs[iter] <- nloglik_marginal(mu, cens, alpha, lambdahat_0, Lambdahat_0, g_lambdahat_0, g_Lambdahat_0)
 beta_cur <- creg_naive$coef
 # marginal approach
 while(iter < maxiter){
   num <- (1-alpha) * fymu(mu, cens, lambdahat_0, Lambdahat_0)
-  denom <- num + alpha * fy(lambdahat_0_g, Lambdahat_0_g)
+  denom <- num + alpha * fy(g_lambdahat_0, g_Lambdahat_0)
   pcur <- num/denom
+  #####sum(is.na(pcur)); pcur[pcur != 0]; sum(pcur)
+  ###############Since coxph can't handle 0 weight, so assign 10^-5 to the observation with 0 weight
+  pcur[pcur == 0] = 10^-5 #sum(pcur == 0)
   alpha <- 1 - mean(pcur)
   #################partially likelihood for estimating beta#############################
+  #creg <- coxph(Surv(yperm, event = 1 - cens) ~ X - 1, weights = pcur, subset = (pcur != 0))
+  #creg <- coxph(Surv(yperm[pcur != 0], event = 1 - cens[pcur != 0]) ~ X[pcur != 0,] - 1, weights = pcur[pcur != 0])
   creg <- coxph(Surv(yperm, event = 1 - cens) ~ X - 1, weights = as.vector(pcur))
   #################Breslow Estimator of the Cumulative Baseline Hazard Rate#############
-  Breslow_Estimator <- survfit(creg, type="aalen")
-  Lambdahat_0 <- Breslow_Estimator$cumhaz[match(yperm, Breslow_Estimator$time)]
-  lambdahat_0 <- c(Lambdahat_0[1], diff(Lambdahat_0))
+  #Breslow_Estimator <- survfit(creg, type="aalen")
+  Breslow_Estimator1 <- basehaz(creg, centered=FALSE)
+  #Lambdahat_0 <- Breslow_Estimator$cumhaz[match(yperm, Breslow_Estimator$time)]
+  Lambdahat_0_ <- Breslow_Estimator1$hazard
+  #plot(Breslow_Estimator$time, Breslow_Estimator1$time)
+  #plot(Lambdahat_0, Lambdahat_0_)
+  lambdahat_0 <- diff(c(0,Breslow_Estimator1[,1]))
+  
+  ##################Compared with true baseline harazrd######################
+  plot(lambdahat_0, 4*yperm^3, xlab = "Breslow Estimator of baseline harzard function", ylab = "True baseline harzard function for weibull")
+  plot(Lambdahat_0, yperm^4, xlab = "Breslow Estimator of baseline cumulative harzard function", ylab = "True baseline cumulative harzard function for weibull")
+  #plot(Breslow_Estimator$time,  Lambdahat_0)
   mu <- X %*% creg$coefficients
   beta_cur <- creg$coefficients
   iter <- iter + 1
-  objs[iter] <- nloglik_marginal(mu, cens, alpha, lambdahat_0, Lambdahat_0)
-  
+  objs[iter] <- nloglik_marginal(mu, cens, alpha, lambdahat_0, Lambdahat_0, g_lambdahat_0, g_Lambdahat_0)
   #####check#################
   #(exp(mu) * lambdahat_0)^(1 - cens) *  exp(-exp(mu) * Lambdahat_0)
   #length(mu); length(lambdahat_0)
   if(objs[iter] + tol > objs[iter-1])
     break
 }
+#objs
 abs(alpha - 0.2)
 sqrt(sum(-beta_cur*sigma - beta)^2)
