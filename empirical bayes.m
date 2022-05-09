7d87de8a7f9623b2d02238290d41f7e21683e11d
Zhenbang(Bruce) Wang <bruce1edward@gmail.com>
2022-05-09 18:27:50 -0400

    Add files via upload

--- empirical bayes.m
+++ empirical bayes.m
@@ -1,4 +1,4 @@
-rng('default')
+rng(123)
 addpath('..\functions')
 iters = 20;iter = 400; % number of iteration for EM alogrithm
 mcmc_steps = 8000; burn_steps = 4000; 
@@ -22,19 +22,30 @@ order = 1:n;
 order = 1:n;
 theta = Choose_theta(n,K);
 [beta_EMM, sigma_EMM] =  EM_mal_tricks(Y_P, X, iter, mcmc_steps, burn_steps, theta, beta_naive, order);
-
-
-function [B_hat2, sigma_sq] = EM_mal_tricks_EB(Y_permuted, X, iters, mcmc_steps, burn_steps, theta, beta_start, order)
+%EM empirical bayes
+%k = 0:1:n;
+log_psi = @(gamma) exp(gamma) - 1 - gamma*n
+track_theta = zeros(iters,1);
+%plot(k, log_psi(k))
+%function [B_hat2, sigma_sq] = EM_mal_tricks_EB(Y_permuted, X, iters, mcmc_steps, burn_steps, theta, beta_start, order)
+%Y_permuted = Y_P; iters = iter; beta_start = beta_naive; order = 1:n; theta = Choose_theta(n,K);
 n = numel(order);
 B_hat2 = beta_start; % initial OLS estimator of B
 sigma_sq = norm(Y_permuted - X*B_hat2)^2/n; % initial OLS estimator of sigma^2
 for k = 1 : iters
         Y_hat = X*B_hat2/sigma_sq; % Computing the estimator of Y_hat
-        [hat_Pi,order] = mcmc_mex_mal(Y_permuted, Y_hat, order, mcmc_steps, burn_steps, theta);\
+        [hat_Pi,order] = mcmc_mex_mal(Y_permuted, Y_hat, order, mcmc_steps, burn_steps, theta);
+        hat_Pi = hat_Pi/(mcmc_steps - burn_steps);
         Pi_Y = hat_Pi*Y_permuted;
         B_hat2 = X\(hat_Pi*Y_permuted);    % Get the Least square solution 
         sigma_sq = norm(Pi_Y - X*B_hat2)^2/n;
-        fun = @(gamma) log(poisscdf(n-K,exp(gamma))/poisscdf(n,exp(gamma)-1)) + gamma*trace(hat_Pi, eye(n))
-        theta = fminbnd(fun,x1,x2);
+        fun = @(gamma) log_psi(gamma) + gamma*sum(diag(hat_Pi)~= 1);
+        theta = fminbnd(fun,0,2*log(n));
+        track_theta(k) = theta;
 end
-end
+%end
+
+ norm(beta_naive - beta)/b
+ norm(beta_EMM  - beta)/b
+ norm(B_hat2  - beta)/b
+ norm(beta_EM  - beta)/b
